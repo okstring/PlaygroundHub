31fd9e7e8862722ee15f6c5b67faa5a1e8db2540
okstring <milkoi7801@gmail.com>
2021-10-18 11:46:17 +0900

    Initial Commit

---
+++ GitInsightHub/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GitInsightHub
+//
+//  Created by Issac on 2021/10/18.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
