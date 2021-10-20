0ea0bc1766e862c996e5116fa51e7e5fd4af4d42
okstring <milkoi7801@gmail.com>
2021-10-21 00:46:31 +0900

    Feat: authorized에 따른 driver output 처리

--- GitInsightHub/Module/Tabs/TabsViewModel.swift
+++ GitInsightHub/Module/Tabs/TabsViewModel.swift
@@ -6,13 +6,37 @@
 //
 
 import Foundation
+import RxSwift
+import RxCocoa
 
 class TabsViewModel: ViewModel, ViewModelType {
     struct Input {
-        
+        let trigger: Observable<Void>
     }
     
     struct Output {
-        
+        let tabBarItems: Driver<[TabBarItem]>
     }
+    
+    let authorized: Bool
+    
+    init(authorized: Bool, usecase: GithubAPI, sceneCoordinator: SceneCoordinatorType) {
+        self.authorized = authorized
+        super.init(usecase: usecase, sceneCoordinator: sceneCoordinator)
+    }
+    
+    func transform(input: Input) -> Output {
+        
+        let tabBarItems = Observable.zip(input.trigger, Observable.just(authorized))
+            .map({ _, authorized -> [TabBarItem] in
+                if authorized {
+                    return [.trand, .profile]
+                } else {
+                    return [.trand, .login]
+                }
+            }).asDriver(onErrorJustReturn: [])
+        
+        return Output(tabBarItems: tabBarItems)
+    }
+    
 }
