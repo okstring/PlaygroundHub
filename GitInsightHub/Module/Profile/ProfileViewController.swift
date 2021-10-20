0f456d043b9e658b9f36549ed8f1c36f42e9aa01
okstring <milkoi7801@gmail.com>
2021-10-21 00:45:37 +0900

    Simple: ProfileViewModel, ViewController 생성

---
+++ GitInsightHub/Module/Profile/ProfileViewController.swift
@@ -0,0 +1,34 @@
+//
+//  ProfileViewController.swift
+//  GitInsightHub
+//
+//  Created by Issac on 2021/10/21.
+//
+
+import UIKit
+
+class ProfileViewController: UIViewController, ViewModelBindableType {
+    var viewModel: ProfileViewModel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    func bindViewModel() {
+        
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ GitInsightHub/Module/Profile/ProfileViewModel.swift
@@ -0,0 +1,23 @@
+//
+//  ProfileViewModel.swift
+//  GitInsightHub
+//
+//  Created by Issac on 2021/10/21.
+//
+
+import Foundation
+
+class ProfileViewModel: ViewModel, ViewModelType {
+    struct Input {
+        
+    }
+    
+    struct Output {
+        
+    }
+    
+    func transform(input: Input) -> Output {
+        
+    }
+    
+}
