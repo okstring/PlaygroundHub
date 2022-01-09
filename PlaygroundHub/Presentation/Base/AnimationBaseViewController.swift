7338ff594628a6527cc791cfb0e49f77cfb9b8e1
okstring <milkoi7801@gmail.com>
2022-01-10 00:00:21 +0900

    Feat: 햅틱 피드백 추가

--- PlaygroundHub/Presentation/Base/AnimationBaseViewController.swift
+++ PlaygroundHub/Presentation/Base/AnimationBaseViewController.swift
@@ -11,6 +11,7 @@ import RxSwift
 
 class AnimationBaseViewController: UIViewController {
     let starredToast = PublishSubject<Bool>()
+    let impactFeedbackGenerator = UIImpactFeedbackGenerator()
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -19,6 +20,7 @@ class AnimationBaseViewController: UIViewController {
     
     func bind() {
         starredToast
+            .do(onNext: { [weak self] _ in self?.impactFeedbackGenerator.impactOccurred() })
             .subscribe(onNext: { isStarred in
                 let vc = UIHostingController(rootView: StarredAnimationView(isStarred: isStarred))
                 guard let alertToast = vc.view else {
